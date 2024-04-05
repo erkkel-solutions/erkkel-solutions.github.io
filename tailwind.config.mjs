4c799ffa0f781362671bf2e19bb27a69030d2324
György Pető <10764203+gpeto86@users.noreply.github.com>
2024-04-05 14:12:23 +0200

    Added basic layouting for landing page (#1)
    * Cleanup and basic navigation layout
    
    * WIP crosses in corners
    
    * Created components for cross and made it more dynamic
    
    * Fix position of crosses
    
    * Added logo
    
    * Added fonts, colors, landing page basic layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,50 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: ['"Space Mono"', '"JetBrains Mono Variable"', ...defaultTheme.fontFamily.mono],
+      display: ['"JetBrains Mono Variable"', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        orange: {
+          100: '#ffefe6',
+          200: '#ffd8c2',
+          300: '#ffb991',
+          400: '#ff985e',
+          500: '#ff792e',
+          600: '#ff5c00',
+          700: '#d94e00',
+          800: '#b54100',
+          900: '#913400',
+          950: '#732900',
+        },
+        gray: {
+          100: '#eaeaea',
+          200: '#cccccc',
+          300: '#a4a4a4',
+          400: '#797979',
+          500: '#515151',
+          600: '#2b2b2b',
+          700: '#252525',
+          800: '#1f1f1f',
+          900: '#131313',
+          950: '#0f0f0f',
+        },
+        // primary: '#ff5c00',
+        // onPrimary: '#000000',
+        // secondary: '#0f0f0f',
+        // onSecondary: '#eaeaea',
+        // tertiary: '',
+        // error: '',
+        // outline: '#2b2b2b',
+      },
+    },
   },
   plugins: [],
 };
