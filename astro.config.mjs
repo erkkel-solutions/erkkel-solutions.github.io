eeefe72198bd0d071caee038b2950622e0646e71
György Pető <10764203+gpeto86@users.noreply.github.com>
2024-04-05 14:24:27 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({	
+	site: 'https://erkkel-solutions.github.io',
+	base: 'website',
   integrations: [tailwind()],
 });
