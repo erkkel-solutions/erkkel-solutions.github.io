79cca81d4700d7fd26107c79c85fbb274ae80803
György Pető <10764203+gpeto86@users.noreply.github.com>
2024-04-05 20:47:44 +0200

    Add compass to landing page (#3)
    * lowercase
    
    * Usage of astro-icon
    
    * renaming
    
    * added compass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({	
-	site: 'https://erkkel-solutions.github.io',
-  integrations: [tailwind()],
+export default defineConfig({
+  site: 'https://erkkel-solutions.github.io',
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
 });
