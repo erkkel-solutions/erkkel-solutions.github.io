301ab0e56315631196bac77bbac8c4eaf740c215
György Pető <10764203+gpeto86@users.noreply.github.com>
2024-04-08 19:37:19 +0200

    moving snake WIP

--- src/utils/snake.ts
+++ src/utils/snake.ts
@@ -9,28 +9,32 @@ function getElement(graph: Graph, index: string) {
 
 const ground = document.getElementById('snake-ground');
 
-export default function moveSnake() {
-  function buildGraph(): Graph {
-    const nodes = Array.from(document.querySelectorAll<HTMLElement>('.cross'));
-    const graph = new Graph({ multi: true });
-    nodes.forEach((node, index) => {
-      graph.addNode(index.toString(), { element: node });
-    });
+function buildGraph(): Graph {
+  const nodes = Array.from(document.querySelectorAll<HTMLElement>('.cross'));
+  const graph = new Graph({ multi: true });
+  nodes.forEach((node, index) => {
+    graph.addNode(index.toString(), { element: node });
+  });
 
-    nodes.forEach((nodeA, indexA) => {
-      nodes.forEach((nodeB, indexB) => {
-        if (indexA !== indexB) {
-          const rectA = nodeA.getBoundingClientRect();
-          const rectB = nodeB.getBoundingClientRect();
+  nodes.forEach((nodeA, indexA) => {
+    nodes.forEach((nodeB, indexB) => {
+      if (indexA !== indexB) {
+        const rectA = nodeA.getBoundingClientRect();
+        const rectB = nodeB.getBoundingClientRect();
 
-          if (rectA.left === rectB.left || rectA.top === rectB.top) {
-            graph.addUndirectedEdge(indexA.toString(), indexB.toString());
-          }
+        if (rectA.left === rectB.left || rectA.top === rectB.top) {
+          graph.addUndirectedEdge(indexA.toString(), indexB.toString());
         }
-      });
+      }
     });
+  });
 
-    return graph;
+  return graph;
+}
+
+export default function moveSnake() {
+  if (!ground) {
+    return;
   }
 
   const graph = buildGraph();
@@ -39,21 +43,45 @@ export default function moveSnake() {
   if (!path) {
     return;
   }
+  const lines = generateLines(graph, path);
 
-  generateLines(graph, path);
+  const timeline = anime.timeline({
+    duration: 2000,
+    easing: 'linear',
+  });
 
-  // anime({
-  //   targets: element,
-  //   keyframes: [{ translateY: -40 }, { translateX: 250 }, { translateY: 40 }, { translateX: 0 }, { translateY: 0 }],
-  //   duration: 4000,
-  //   loop: true,
-  // });
+  const horizontalKeyFrames = [
+    {
+      backgroundPositionX: '100%',
+      backgroundPositionY: 0,
+    },
+  ];
+  const verticalKeyFrames = [
+    {
+      backgroundPositionX: 0,
+      backgroundPositionY: '100%',
+    },
+  ];
+
+  lines.forEach((line) => {
+    timeline.add({
+      targets: line,
+      keyframes: isLineHorizontal(line) ? horizontalKeyFrames : verticalKeyFrames,
+    });
+  });
 }
 
-function generateLines(graph: Graph, path: ShortestPath) {
+function isLineHorizontal(line: HTMLDivElement) {
+  return line.style.height === '2px' || line.style.height === '';
+}
+
+function generateLines(graph: Graph, path: ShortestPath): HTMLDivElement[] {
   const offset = 7.5;
   let prevIndex = 0;
-  for (let i = 1; i <= path.length; i++) {
+
+  const array: HTMLDivElement[] = [];
+
+  for (let i = 1; i < path.length; i++) {
     const prev = getElement(graph, path[prevIndex]).getBoundingClientRect();
     const current = getElement(graph, path[i]).getBoundingClientRect();
 
@@ -65,23 +93,24 @@ function generateLines(graph: Graph, path: ShortestPath) {
       // horizontal
       const width = Math.abs(current.x - prev.x);
       line.style.width = `${width}px`;
+      line.classList.add('hline');
     } else {
       // vertical
       const height = Math.abs(current.y - prev.y);
       line.style.height = `${height}px`;
+      line.classList.add('vline');
     }
 
     prevIndex = prevIndex + 1;
+    array.push(line);
   }
+
+  return array;
 }
 
 function createLine(top: string, left: string): HTMLDivElement {
   const line = document.createElement('div');
-  line.style.position = 'absolute';
-  line.style.zIndex = '90';
-  line.style.backgroundColor = 'orange';
-  line.style.height = '2px';
-  line.style.width = '2px';
+  line.className = 'line';
   line.style.top = top;
   line.style.left = left;
   ground!.appendChild(line);
